6e7adb10c2554301e4c7e79c47d706ff030b02c6
Angélica Cavalheiro Rodrigues <angelicacavalheiro51@gmail.com>
2021-12-12 22:42:36 -0300

    data formatting added

--- src/controllers/questionController.ts
+++ src/controllers/questionController.ts
@@ -51,13 +51,25 @@ async function postQuestion(req: Request, res: Response) {
 }
 
 async function getQuestions(req: Request, res: Response) {
-  const result = await connection.query(`
+  const questions = await connection.query(`
   SELECT questions.id, question, student, class.class_name as "class", "submitAt"
     FROM questions JOIN class ON questions.class_id = class.id
   WHERE answered = false;
   `);
 
-  res.status(200).send(result.rows)
+  const questiosFormated = questions.rows.map(question => (
+    {
+      id: question.id,
+      question: question.question,
+      student: question.student,
+      class: question.class,
+      submitAt: (question.submitAt).substring(0, (question.submitAt).length-13).replace(/[T]/, ' ')
+    }
+  ))
+
+  // saida = entrada.substring(0, entrada.length-13).replace(/[T]/, ' ')
+
+  res.status(200).send(questiosFormated)
 }
 
 async function answer(req: Request, res: Response) {
@@ -91,21 +103,47 @@ async function getOneQuestion (req: Request, res: Response) {
 
   const answered = await connection.query(`SELECT answered FROM questions WHERE id = $1;`, [id]);
   if (answered.rows[0].answered === false) {
-    const result = await connection.query(`
+    const questions = await connection.query(`
       SELECT question, student, class.class_name as "class", tags, answered, "submitAt"
         FROM questions JOIN class ON questions.class_id = class.id
       WHERE questions.id = $1;
     `, [id]);
-    return res.status(200).send(result.rows[0])
+
+    const questiosFormated = questions.rows.map(question => (
+      {
+        question: question.question,
+        student: question.student,
+        class: question.class,
+        tags: question.tags,
+        answered: question.answered,
+        submitAt: (question.submitAt).substring(0, (question.submitAt).length-13).replace(/[T]/, ' ')
+      }
+    ))
+
+    return res.status(200).send(questiosFormated)
   }
-  const result = await connection.query(`
+  const questions = await connection.query(`
     SELECT question, student, class.class_name as "class", tags, answered, "submitAt",
     "answeredAt", "user".user_name as "answeredBy", "answer"
       FROM questions JOIN class ON questions.class_id = class.id
       JOIN "user" ON questions.token_replied = "user".token
     WHERE questions.id = $1;
     `, [id]);
-  res.status(200).send(result.rows[0])
+
+    const questiosFormated = questions.rows.map(question => (
+      {
+        question: question.question,
+        student: question.student,
+        class: question.class,
+        tags: question.tags,
+        answered: question.answered,
+        submitAt: (question.submitAt).substring(0, (question.submitAt).length-13).replace(/[T]/, ' '),
+        answeredAt: (question.answeredAt).substring(0, (question.answeredAt).length-13).replace(/[T]/, ' '),
+        answeredBy: question.answeredBy,
+        answer: question.answer
+      }
+    ))
+  res.status(200).send(questiosFormated)
 }
 
 export {
